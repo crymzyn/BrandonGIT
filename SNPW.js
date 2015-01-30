f42f39a043975ca90ab377c2a06c03fa5f911fe0
crymzyn <crymzyn@gmail.com>
2015-01-30 17:41:47 -0600

    Updated SNPW.js

--- SNPW.js
+++ SNPW.js
@@ -1,4 +1,4 @@
-
+S_ServiceNowRB_US / S3rv1c3N0wU$
 
 S_ServiceNowRB_MNETe / S3rv1c3N0wMN3+3
 
@@ -9,4 +9,3 @@ S_ServiceNowRB_MPNe / S3rv1c3N0wM%N3
 S_ServiceNowRB_MPNi / S3rv1c3N0wM%N1
 
 S_ServiceNowRB_MPN / S3rv1c3N0wM%N
-
